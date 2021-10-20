522f1afdb6458ed87f6a78e246e1f5acee6bfe31
Ester <ester_sgomes@hotmail.com>
2021-10-20 17:48:25 -0300

    first commit

---
+++ node_heat/README.md
@@ -0,0 +1,25 @@
+
+# NLW / heat 🔥💥🔥
+
+
+1. Express + Typescript
+2. Prima
+
+  → yarn add prisma -D
+  → yarn prisma init
+
+- SQLite
+    
+  → 
+
+- Github
+  
+  → Settings/ Developer settings / OAuth app
+  → Rota de login
+  → Rota de callback
+  → Authentificar 
+  → Cadastro mensagem
+
+- Socket
+
+  → yarn add socket.io
---
+++ node_heat/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import {Request, Response, NextFunction} from 'express'
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string
+}
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+
+  const authToken = req.headers.authorization;
+
+  if(!authToken){ 
+    return res.status(401).json({
+      errorCode: "token.invalid"
+    });
+  }
+
+  // Bearer 0909as0d848a40d9asd
+  // [0] Bearer
+  // [1] 909as0d848a40d9asd
+
+  const [, token] = authToken.split(" ")
+
+  try {
+    
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    req.user_id = sub;
+
+    return next();
+
+  } catch (error) {
+    
+    return res.status(401).json({ errorCode: "token.expired" })
+
+  }
+
+}
